a99f8ffd0cabbab28ab7beb9dded5eb6e9f05c36
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-10-10 15:47:53 +0100

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-'''Task 12: Type Checking
-'''
+"""
+Task 12: Type Checking
+"""
+
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    '''Creates multiple copies of items in a tuple.
-    '''
-    zoomed_in: List = [
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """
+    Creates multiple copies of items in a tuple.
+
+    Args:
+        lst (Tuple[int, ...]): The tuple to be copied.
+factor (int): The number of times each item will be repeated. Default is 2.
+
+    Returns:
+        List[int]: A list with items repeated 'factor' times.
+    """
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
